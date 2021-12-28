251335c171d3d56bb59c1f56fa1c91366e6f2168
adeirjunior <73009242+adeirjunior@users.noreply.github.com>
2021-12-28 14:22:46 -0400

    started

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navigator from './containers/Navigator';
+
+
+
+function App() {
+  return (
+    <>
+    <Navigator />
+    </>
+  );
+}
+
+export default App;
---
+++ src/containers/Navigator.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import { ReactComponent as Logo } from '../assets/icons/monetary.svg';
+
+const NavWrapper = styled.div`
+    width: 100%;
+    border: solid white 1px;
+    box-sizing: border-box;
+`
+const Container = styled.div`
+
+`
+const Nav = styled.nav`
+    width:100%;
+    height: 20px;
+    background-color: #DD1C1A;
+`
+
+const Span = styled.span`
+    padding: 2em 1em;
+    background-color: #DD1C1A;
+`
+
+function Navigator() {
+    return (
+        <NavWrapper>
+            <Nav />
+            <Span><Logo/></Span>
+        </NavWrapper>
+    )
+}
+
+export default Navigator
